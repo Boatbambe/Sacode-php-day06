80ddaf4a59e930209cdb79b2f6323f60f17ca058
BoatBambe <120298072+Boatbambe@users.noreply.github.com>
2023-07-04 18:14:09 +0700

    Om Update

---
+++ include/nav.php
@@ -0,0 +1,9 @@
+<nav class  ="border border-gray-800 container mx-auto p-6 text-center">
+        <a href="../pages/index.php" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition" > Beranda</a>
+        <a href="" class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Teknologi</a>
+        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Pendidikan</a>
+        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Pengembangan Diri</a>
+        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Kursus</a>
+        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Komunitas</a>
+        <a href=""  class="border py-3 px-4 bg-gray-200 hover:bg-gray-300 transition"> Lainnya</a>
+    </nav>   
